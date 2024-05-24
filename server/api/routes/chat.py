2eba517dab484f52062faaede4dc5f966cda13dd
Sebastian <135080773+ImBoolean-2@users.noreply.github.com>
2024-05-23 22:45:09 -0500

    Test chat.py
    Change APIKEY for our own GPT4 API

--- server/api/routes/chat.py
+++ server/api/routes/chat.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from openai import GPT4
 #from api.functions import interactions
 
 chat_bp = Blueprint('chat', __name__)
 
+# gpt4 = GPT4('APIKEY')
 @chat_bp.route('/chat', methods=['POST'])
 def chat():
-    # ...
-    pass
+    user_input = request.json.get('Mensaje | Input: ', '')
+    response = GPT4.generate(user_input)
+    return jsonify({'Levus | Output ': response})
