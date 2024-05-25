4c27bc3740e38ca99c02008cd4e607dbce11da17
Just253 <70507145+Just253@users.noreply.github.com>
2024-05-25 01:21:43 -0500

    Fix: close_window.py

--- server/commands/close_window.py
+++ server/commands/close_window.py
@@ -1,35 +1,11 @@
-import OS
-from Command Close_window.py import Close_window
-
-class Bot_Close_window(Close_window):
-    def __init__(self):
-        super().__init__()
-
-    def execute(self):
-    os.system("taskkill /f /im notepad.exe")
-    pass
-    
-    def getName(self):
-        return self.name
-
-    def getDescription(self):
-        return self.description
-
-    def getRequeriments(self):
-        return self.requeriments
-
-    def getKeywords(self):
-        return self.keywords
-
-    def setName(self, name):
-        self.name = newname
-
-    def setDescription(self, description):
-        self.description = new_description
-
-    def setRequeriments(self, requeriments):
-        self.requeriments = new_requeriments
-
-    def setKeywords(self, keywords):
-        self.keywords = new_keywords
+from command import Command
+class BotCommand(Command):
+    name="close_window"
+    description="este comando cierra una ventana"
+    def execute(self, processName):
+        # TODO: Implementar el cierre de la ventana mendiante nombre/ID del proceso
+        # if is int -> kill by ID
+        # if is str -> kill by name
+        import os
+        os.system(f"taskkill /f /im {processName}")
 
--- server/commands/command_close_window.py
+++
@@ -1,8 +0,0 @@
-class Close_window:
-    name="close window"
-    description="este comando cierra una ventana"
-    requeriments=OS
-    keywords=None 
-    pass
-
-
