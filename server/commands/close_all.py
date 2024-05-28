f550bb8671074b07dde237b678dff18a8ba6fba2
Just253 <70507145+Just253@users.noreply.github.com>
2024-05-27 23:56:21 -0500

    feat: Agregar comando para ocultar todas las ventanas

--- server/commands/close_all.py
+++
@@ -1,9 +0,0 @@
-from command import Command
-class BotCommand(Command):
-    name="close_all"
-    description="este comando cierra todas las tareas en proceso "
-    def execute(self):
-        import os
-        os.system('taskkill /F /FI "STATUS eq RUNNING"')
-        
-
---
+++ server/commands/hide_all.py
@@ -0,0 +1,14 @@
+from command import Command
+import ctypes
+
+class BotCommand(Command):
+    name="hide_all"
+    description="este comando oculta todas las ventanas"
+    
+    def execute(self):
+        # presionar tecla
+        ctypes.windll.user32.keybd_event(0x5B, 0, 0, 0)  # Tecla Windows abajo
+        ctypes.windll.user32.keybd_event(0x44, 0, 0, 0)  # Tecla D abajo
+        # soltar tecla
+        ctypes.windll.user32.keybd_event(0x44, 0, 2, 0)  # Tecla D arriba
+        ctypes.windll.user32.keybd_event(0x5B, 0, 2, 0)  # Tecla Windows arriba
