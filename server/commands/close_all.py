a4721798cae48db92aa64164314c7cb6abfd734f
carltofen <167097295+carltofen@users.noreply.github.com>
2024-05-27 23:30:24 -0500

    le quite el procces name que se colo ahi

--- server/commands/close_all.py
+++ server/commands/close_all.py
@@ -2,7 +2,7 @@ from command import Command
 class BotCommand(Command):
     name="close_all"
     description="este comando cierra todas las tareas en proceso "
-    def execute(self, processName):
+    def execute(self):
         import os
         os.system('taskkill /F /FI "STATUS eq RUNNING"')
         
