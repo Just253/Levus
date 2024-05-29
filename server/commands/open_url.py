2e49b19c36585c3261ca9af3e456a908ef3300ef
Just253 <70507145+Just253@users.noreply.github.com>
2024-05-29 16:49:15 -0500

    refactor: New CommandHandler to get tool to OpenAI

--- server/commands/open_url.py
+++ server/commands/open_url.py
@@ -1,8 +1,13 @@
-from command import Command
-
+from .command import Command
 class BotCommand(Command):
     name = "open_url"
-    description = "Opens a URL in the default browser"
-    def execute(self, url):
+    description = "Abre una url en el navegador"
+    def execute(self, url: str, new_tab: bool = False):
+        """
+        :param url: Url a abrir
+        :type url: string
+        :param new_tab: Abre en una nueva pestaña
+        :type new_tab: boolean
+        """
         import os
         os.system(f"start {url}")
