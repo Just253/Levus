f50786b8a4b0c4bb296af927b7028f1aaae03f1c
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-01 06:33:19 -0500

    refactor: Update get_response_from_openai function to include streaming and status updates

--- server/api/functions/interactions.py
+++ server/api/functions/interactions.py
@@ -1,9 +1,10 @@
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from openai import OpenAI
 from flask import current_app as app
+from ..routes.status import update_status
 
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-def get_response_from_openai(messages, tools=None, tool_choice=None, model="gpt-3.5-turbo"):
+def get_response_from_openai(messages, process_id, tools=None, tool_choice=None, model="gpt-3.5-turbo"):
     client = OpenAI(api_key=app.config["OPENAI_API_KEY"])
     system_message = list(filter(lambda x: x['role'] == 'system', messages))[0]
     non_system_messages = list(filter(lambda x: x['role'] != 'system', messages))
@@ -13,9 +14,18 @@ def get_response_from_openai(messages, tools=None, tool_choice=None, model="gpt-
         response = client.chat.completions.create(
             model=model,
             messages=messages,
+            temperature=0.2,
+            stream=True,
         )
-        return response.choices[0].message.content
+        chunk_messages = ""
+        for chunk in response:
+            message = chunk.choices[0].delta.content
+            if message != None:
+                chunk_messages += message
+                update_status(process_id=process_id, response=chunk_messages)
+        update_status(process_id=process_id, status="completed")
     except Exception as e:
+        update_status(process_id=process_id, status="error", error=str(e))
         print("Unable to generate ChatCompletion response")
         print(f"Exception: {e}")
         return str(e)
