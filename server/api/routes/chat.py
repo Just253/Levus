339bdc0e82979aa84a5ef78595fa9fe4a0040084
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-02 03:46:40 -0500

    refactor: Update chat route to use Flask-Executor for asynchronous processing

--- server/api/routes/chat.py
+++ server/api/routes/chat.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, jsonify
-from ..functions.interactions import get_response_from_openai
-import uuid
-from .status import create_status
 from flask import current_app as app
 from flask_executor import Executor
+from ..functions.interactions import get_response_from_openai
+from ..functions.db import statusTable
+import uuid
 
 chat_bp = Blueprint('chat', __name__)
 
@@ -20,7 +20,8 @@ def chat():
     if isinstance(model, list):
         model = model[0]
     process_id = str(uuid.uuid4())
-    create_status(process_id)
+    table = statusTable("processes", app)
+    table.create_status(process_id)
     executor = Executor(app)
-    executor.submit(get_response_from_openai, messages, process_id, model=model)
+    executor.submit(get_response_from_openai, messages, process_id,table=table, model=model)
     return jsonify({'process_id': process_id})
