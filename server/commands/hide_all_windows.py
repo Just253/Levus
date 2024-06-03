32f4d9bf51a4d81b4a835933fa43f4e331dc568c
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-03 04:36:27 -0500

    refactor: Rename hide_all.py to hide_all_windows.py and update BotCommand name to hide_all_windows

--- server/commands/hide_all_windows.py
+++ server/commands/hide_all_windows.py
@@ -2,7 +2,7 @@ from .command import Command
 import ctypes
 
 class BotCommand(Command):
-    name="hide_all"
+    name="hide_all_windows"
     description="este comando oculta todas las ventanas"
     
     def execute(self):
