f045b2947eb725d4fe87b19e3116ee51f1d023e5
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-06 16:24:30 -0500

    refactor: Update cv2.VideoCapture to use CAP_DSHOW for Windows compatibility

--- tests/cam/print_landmark.py
+++ tests/cam/print_landmark.py
@@ -7,7 +7,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_drawing = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 last_saved_time = time.time()
 
 try:
