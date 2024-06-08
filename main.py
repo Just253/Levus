ed4f4657cb2640abb91d2bee1759ba99526fd62e
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-08 16:01:13 -0500

    feat: Add socketio event logging in main.py

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from server import create_app, socketio
 app = create_app()
 if __name__ == "__main__":
     print(app.url_map)
+    for namespace, events in socketio.server.handlers.items():
+        print(f'Namespace: {namespace}')
+        for event in events.keys():
+            print(f'  Event: {event}')
     socketio.run(app)
