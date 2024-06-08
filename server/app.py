6cda74dc5df1b9c0c8ed7cbc5644def4cab4efaa
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-08 11:40:05 -0500

    refactor: Update Flask app initialization and configuration

--- server/app.py
+++ server/app.py
@@ -1,29 +1,34 @@
 from flask import Flask, Blueprint
+from flask_socketio import SocketIO
 from dotenv import load_dotenv
 import os
 import importlib
 from .api.functions.commandHandler import CommandHandlerObserver
 
 load_dotenv()
-app = Flask(__name__)
-app.config["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+socketio = SocketIO()
 
-# Importar automáticamente todas las clases de los directorios de api/routes/
+def create_app(debug=False):
+    app = Flask(__name__)
+    app.debug = debug
+    app.config["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    # Importar automáticamente todas las clases de los directorios de api/routes/
+    routes_dir = os.path.join(os.path.dirname(__file__), 'api', 'routes')
+    for filename in os.listdir(routes_dir):
+        if filename.endswith('.py') and filename != '__init__.py':
+            try:
+                module = importlib.import_module(f'server.api.routes.{filename[:-3]}')
+                for attr_name in dir(module):
+                    attr = getattr(module, attr_name)
+                    if isinstance(attr, Blueprint):
+                        app.register_blueprint(attr)
+            except Exception as e:
+                print(f"Error al importar el módulo {filename}: {e}")
 
-routes_dir = os.path.join(os.path.dirname(__file__), 'api', 'routes')
-for filename in os.listdir(routes_dir):
-    if filename.endswith('.py') and filename != '__init__.py':
-        try:
-            module = importlib.import_module(f'server.api.routes.{filename[:-3]}')
-            for attr_name in dir(module):
-                attr = getattr(module, attr_name)
-                if isinstance(attr, Blueprint):
-                    app.register_blueprint(attr)
-        except Exception as e:
-            print(f"Error al importar el módulo {filename}: {e}")
+    observer = CommandHandlerObserver(os.path.join(os.path.dirname(__file__), 'commands'), app)
+    observer.commandHandler.logging = True
 
-observer = CommandHandlerObserver(os.path.join(os.path.dirname(__file__), 'commands'), app)
-observer.commandHandler.logging = True
-def run_app():
     print(app.url_map)
-    app.run(debug=True, port=5000)
+    socketio.init_app(app)
+    return app
+    
