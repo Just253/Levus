4e45bec150151d699bee1c21666def84b4274078
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-10 04:18:45 -0500

    .

--- app/src/main/java/levus/gui/connections/Chat.java
+++ app/src/main/java/levus/gui/connections/Chat.java
@@ -1,4 +1,22 @@
 package levus.gui.connections;
 
+import io.socket.client.Socket;
+
 public class Chat {
+    private Socket_manager socket_manager;
+    private Socket Socket;
+    public Chat(Socket_manager socket_manager) {
+        this.socket_manager = socket_manager;
+        this.setSocket(socket_manager.getSocket());
+        add_listeners();
+
+    }
+
+    public void setSocket(Socket socket) {
+        this.Socket = socket;
+    }
+
+    public void add_listeners() {
+        // Add listeners here
+    }
 }
