e2319ebc261a5d1096594a968cd1638ebfa5ba12
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-10 02:58:11 -0500

    refactor: Update Socket_manager class to handle socket connections and improve connection reliability

--- app/src/main/java/levus/gui/connections/Socket_manager.java
+++ app/src/main/java/levus/gui/connections/Socket_manager.java
@@ -1,7 +1,24 @@
 package levus.gui.connections;
 
-public class Socket {
-    publick static void Socket() {
-        System.out.println("Socket");
+import java.io.IOException;
+import java.net.Socket;
+
+public class Socket_manager {
+    private Socket socket;
+    private String host;
+    private int port;
+
+    public Socket_manager(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void connect() throws IOException {
+        try {
+            socket = new Socket(host, port);
+        } catch (IOException e) {
+            System.out.println("Connection failed | Retrying...");
+            this.connect();
+        }
     }
 }
