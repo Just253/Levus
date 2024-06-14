c13e2903676d20fe1aaf7d69b7f51d19077408aa
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-14 03:46:06 -0500

    feat: Update Cam class to use ImageView for camera display

--- app/src/main/java/levus/gui/connections/Cam.java
+++ app/src/main/java/levus/gui/connections/Cam.java
@@ -1,92 +1,77 @@
 package levus.gui.connections;
 
-import io.socket.client.Socket;
+import javafx.application.Platform;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.application.Platform;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.util.Duration;
+import levus.gui.helper.VideoSource;
+import java.io.ByteArrayInputStream;
 
 public class Cam {
-  private final Socket_manager socket_manager;
-  private Socket socket;
-  private MediaView camView;
-  private MediaPlayer mediaPlayer;
-  public Boolean isOn = false;
-  private ToggleButton toggleCamButton;
+    private final Socket_manager socket_manager;
+    private VideoSource videoSource;
+    private Boolean isOn = false;
+    private ToggleButton toggleCamButton;
+    private ImageView imageView; // Para mostrar imágenes del flujo
 
-  public Cam(Socket_manager socket_manager) {
-    this.socket_manager = socket_manager;
-    this.setSocket(socket_manager.getSocket());
-    this.makeMedia();
-    this.add_listeners();
-  }
-  public void setSocket(Socket socket) {
-    this.socket = socket;
-  }
+    public Cam(Socket_manager socket_manager) {
+        this.socket_manager = socket_manager;
+        this.imageView = new ImageView();
+        this.imageView.setFitWidth(640); // Configurar tamaño preferido
+        this.imageView.setFitHeight(480);
+        this.add_listeners();
+    }
 
-  public Socket getSocket() {
-    return this.socket;
-  }
+    public void add_listeners() {
+        socket_manager.getSocket().on("ToggleCam", args -> {
+            isOn = (Boolean) args[0];
+            toggleCam(isOn);
+        });
+    }
 
-  public void add_listeners() {
-    socket.on("ToggleCam", args -> {
-        isOn = (Boolean) args[0];
-        toggleCam(isOn);
-
-    });
-  }
-
-  public void toggleCam(Boolean isOn) {
-    System.out.println("toggleCam called with: " + isOn);
-    toggleCamButton.setSelected(isOn);
-    Platform.runLater(() -> {
-        VBox contentApp = (VBox) socket_manager.getStage().getScene().lookup("#appContent");
-        if (isOn) {
-            System.out.println("Turning on the camera");
-            if (!contentApp.getChildren().contains(camView)) {
-                System.out.println("Adding camView to contentApp");
-                contentApp.getChildren().add(camView);
+    public void toggleCam(Boolean isOn) {
+        System.out.println("toggleCam called with: " + isOn);
+        toggleCamButton.setSelected(isOn);
+        Platform.runLater(() -> {
+            VBox contentApp = (VBox) socket_manager.getStage().getScene().lookup("#appContent");
+            if (isOn) {
+                System.out.println("Turning on the camera");
+                videoSource = new VideoSource("http://127.0.0.1:5000/stream"); // URL del flujo MJPEG
+                try {
+                    videoSource.connect();
+                    new Thread(() -> {
+                      for (byte[] img : videoSource) {
+                        System.out.println("New image received");
+                        Platform.runLater(() -> {
+                          Image image = new Image(new ByteArrayInputStream(img));
+                          if (!image.isError()) {
+                            imageView.setImage(image);
+                          } else {
+                            System.out.println("Error loading image");
+                            // Manejar el error de carga de la imagen aquí
+                          }
+                        });
+                      }
+                    }).start();
+                    if (!contentApp.getChildren().contains(imageView)) {
+                        System.out.println("Adding imageView to contentApp");
+                        contentApp.getChildren().add(imageView);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Turning off the camera");
+                if (videoSource != null) {
+                    videoSource.disconnect();
+                }
+                contentApp.getChildren().remove(imageView);
             }
-            // Activar la escucha del evento "receiveImage"
-            System.out.println("Playing media");
-            mediaPlayer.play();
-        } else {
-            System.out.println("Turning off the camera");
-            contentApp.getChildren().remove(camView);
-            // Desactivar la escucha del evento "receiveImage"
-            System.out.println("Stopping media");
-            mediaPlayer.stop();
-        }
-    });
-    System.out.println("Emitting toggleCam event with: " + isOn);
-    socket.emit("toggleCam", isOn);
-  }
+        });
+    }
 
-  public void makeMedia() {
-    // TODO: Arreglarlo porque no funciona 
-    String url = "http://127.0.0.1:5000/stream";
-    Media media = new Media(url);
-    mediaPlayer = new MediaPlayer(media);
-    camView = new MediaView(mediaPlayer);
-    camView.setFitWidth(640);
-    camView.setFitHeight(1);
-
-    // Agrega un EventHandler al mediaPlayer
-    mediaPlayer.setOnEndOfMedia(new Runnable() {
-        @Override
-        public void run() {
-            // Cuando el video llega al final, reinicia la reproducción
-            mediaPlayer.seek(Duration.ZERO);
-        }
-    });
-  }
-
-  public void setToggleCamButton(ToggleButton toggleCamButton) {
-    this.toggleCamButton = toggleCamButton;
-  }
+    public void setToggleCamButton(ToggleButton toggleCamButton) {
+        this.toggleCamButton = toggleCamButton;
+    }
 }
