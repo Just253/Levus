4892a46b535e6f49a561df93317813090c9ba2a5
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-15 04:05:42 -0500

    chore: Update flask stream route to improve image streaming

--- server/api/routes/stream.py
+++ server/api/routes/stream.py
@@ -1,40 +1,10 @@
-from flask import Response, Blueprint,render_template_string
-import cv2, time
-
+from flask import Response, Blueprint, render_template_string
+from ...functions import generate
 stream_bp = Blueprint('stream', __name__)
 
 @stream_bp.route('/stream', methods=['GET'])
 def stream():
     print("Stream request")
-    def generate():
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-        fps = 30.0  # FPS deseados
-        frame_time = 1.0 / fps  # Tiempo entre frames
-
-        while True:
-            start_time = time.time()  # Tiempo de inicio del frame
-
-            ret, frame = cap.read()
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            frame = jpeg.tobytes()
-            try:
-                print("Yielding frame")
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n'
-                       b'Content-Length: ' + bytes(str(len(frame)), 'utf-8') + b'\r\n\r\n' +
-                       frame + b'\r\n\r\n')
-            except:
-                print("Closing camera")
-                cap.release()
-                return
-
-            time_elapsed = time.time() - start_time  # Tiempo que tardó en procesar el frame
-            if time_elapsed < frame_time:
-                time.sleep(frame_time - time_elapsed)  # Pausa para mantener los FPS deseados
-        print("Closing camera")
 
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
