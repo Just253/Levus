eeaac909fe642bbce413328a5f2a475d5b55ce06
Just253 <70507145+Just253@users.noreply.github.com>
2024-06-30 21:20:17 -0500

    feat: Integrate OpenAI Whisper for speech recognition test

--- tests/voice/SpeechRecognition/microphone.py
+++ tests/voice/SpeechRecognition/microphone.py
@@ -1,4 +1,7 @@
 import speech_recognition as sr
+import openai
+from io import BytesIO
+
 r = sr.Recognizer()
 with sr.Microphone() as _:
     r.adjust_for_ambient_noise(_)
@@ -10,8 +13,12 @@ while True:
 
   # recognize speech using whisper
   try:
-      print("Whisper thinks you said " + r.recognize_vosk(audio, language="es")) 
-      # Whisper thinks you said Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.
+      
+    wav_data = BytesIO(audio.get_wav_data())
+    wav_data.name = "SpeechRecognition_audio.wav"
+    client = openai.OpenAI(api_key="...")
+    transcript = client.audio.transcriptions.create(file=wav_data, model="whisper-1")
+    print(transcript.text)
   except sr.UnknownValueError:
       print("Whisper could not understand audio")
   except sr.RequestError as e:
